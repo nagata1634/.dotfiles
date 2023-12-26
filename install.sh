5c2da411e4e54fdca8057b754cfba53e74165d68
yuuya <yuuya@pc1.jp>
2023-12-26 14:54:16 +0900

    構成の構造化

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if [ "uname" == "Linux" ]; then
     for tool in "${tools[@]}"; do
         sudo apt-get --yes install "$tool"
     done
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nagata1634/.dotfiles/main/install.sh)"
 elif [ "uname" == "Darwin" ]; then
     xcode-select --install
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
