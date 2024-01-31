4c745c92ef5508f75528023bbeea396bd6bd15e4
Nagata1634 <yuya.yuekaevae@gmail.com>
2024-01-31 23:23:01 +0900

    同期

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,4 @@
--- タブをスペースに変更
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-
--- VimLeave イベントでスワップファイルを削除
-vim.cmd[[
-  augroup RemoveSwapFile
-    autocmd!
-    autocmd VimLeave * silent !rm -f %:p:h/.%:t
-  augroup END
-]]
+-- converting tab to space
+vim.api.nvim_set_option('tabstop', 4)
+vim.api.nvim_set_option('shiftwidth', 4)
+vim.api.nvim_set_option('expandtab', true)
